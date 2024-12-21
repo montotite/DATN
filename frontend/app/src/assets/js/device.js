78cc4fda34a01d96c3a4aa84cab1bfda41f6cd01
vuong0809 <tuanvuong08092000@gmail.com>
2024-12-21 10:24:26 +0700

    fe

--- frontend/app/src/assets/js/device.js
+++ frontend/app/src/assets/js/device.js
@@ -1,9 +1,53 @@
 
+
+function api_device_list() {
+    fetch('http://103.176.251.60:32770/api/device?page_size=10&page=0')
+        .then(response => response.json())
+        .then(data => load_device(data))
+        .catch(error => console.error('Error:', error));
+}
+
+const form_create_device = document.getElementById("form_create_device");
 const device_ls = document.getElementById("device_ls");
 
+form_create_device.addEventListener("submit", create_device)
+async function create_device(event) {
+    const name = document.getElementById("name");
+    const description = document.getElementById("description");
+    console.log(name.value)
+
+    const rawResponse = await fetch('http://103.176.251.60:32770/api/device', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "name": name.value
+        })
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+    const item = document.createElement('div');
+    item.className = "col-lg-3"
+    item.innerHTML = `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${content.name}</h5>
+                <p class="card-text">
+                Some quick example text to build on the card title and make up
+                                        the bulk of
+                                        the
+                                        card's content.
+                </p>
+                <a href="#" class="card-link">Chi tiết</a>
+            </div>
+        </div>    
+        `
+    device_ls.appendChild(item)
+}
 function load_device(data) {
     data.data.forEach(element => {
-        console.log(element.name)
         const item = document.createElement('div');
         item.className = "col-lg-3"
         item.innerHTML = `
@@ -11,9 +55,10 @@ function load_device(data) {
             <div class="card-body">
                 <h5 class="card-title">${element.name}</h5>
                 <p class="card-text">
-                
-                
-                
+                Some quick example text to build on the card title and make up
+                                        the bulk of
+                                        the
+                                        card's content.
                 </p>
                 <a href="#" class="card-link">Chi tiết</a>
             </div>
@@ -23,7 +68,5 @@ function load_device(data) {
     });
 }
 
-fetch('http://103.176.251.60:32770/api/device?page_size=10&page=0')
-    .then(response => response.json())
-    .then(data => load_device(data))
-    .catch(error => console.error('Error:', error));
+
+api_device_list();
