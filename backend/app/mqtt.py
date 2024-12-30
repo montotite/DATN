0d3952be8e7fba5ca98350a73b60b0f423172f2a
vuong0809 <tuanvuong08092000@gmail.com>
2024-12-30 21:35:25 +0700

    mqtt

--- backend/app/mqtt.py
+++ backend/app/mqtt.py
@@ -1,14 +1,14 @@
 import json
 import sys
 import os
+import time
 
-# import threading
 # import paho.mqtt as mqttc
-import paho.mqtt.client as paho
+import paho.mqtt.client as mqttClinet
 
 from threading import Thread
 
-from utils import *
+from utils import Crud, basic_publish, timestamp, generate_uuid
 from schemas import AttributesScope
 from helpers import (
     MqttTopic,
@@ -42,12 +42,10 @@ def save_telemetry(payload, device_info, ts):
             "device_info": device_info,
         }
         msg = json.dumps(msg)
-        basic_publish(Queue.SAVE_TELEMETRY, msg)
+        basic_publish(channel, Queue.SAVE_TELEMETRY, msg)
 
 
 def save_attibute(payload, device_info, ts):
-    pass
-
     for item in payload.keys():
         msg = {
             "payload": {"key": item, "value": payload[item]},
@@ -56,7 +54,7 @@ def save_attibute(payload, device_info, ts):
             "device_info": device_info,
         }
         msg = json.dumps(msg)
-        basic_publish(Queue.SAVE_ATTRIBUTE, msg)
+        basic_publish(channel, Queue.SAVE_ATTRIBUTE, msg)
 
 
 def attibute_req(msg, device_info, ts):
@@ -74,29 +72,33 @@ db = get_db()
 
 
 def message_handling(client, userdata, msg):
-    if len(msg.topic) > 20:
-        topic, token = get_topic(msg)
-        payload = msg.payload.decode()
-        if topic != MqttTopic.ATTRIBUTE_RES:
-            try:
+    try:
+        state = "1"
+        if len(msg.topic) > 20:
+            topic, token = get_topic(msg)
+            payload = msg.payload.decode()
+            state = "2"
+
+            if topic != MqttTopic.ATTRIBUTE_RES:
                 payload = json.loads(payload)
                 ts = timestamp()
                 device_info = db.get_device_info_by_credential(token)
-                # print(device_info)
+                state = "3"
                 if device_info != False:
                     if topic == MqttTopic.TELEMETRY:
+                        state = "4"
                         save_telemetry(payload, device_info, ts)
                     elif topic == MqttTopic.ATTRIBUTE:
+                        state = "5"
                         save_attibute(payload, device_info, ts)
                     elif topic == MqttTopic.ATTRIBUTE_REQ:
                         print(topic)
                     else:
                         print(f"{msg.topic}: {msg.payload.decode()}")
-            except:
-                state = msg
-                logging.error(f"Worker Failed in stage {state.ljust(20, '-')}")
-            finally:
-                pass
+    except:
+        logging.error(f"MQTT Failed in stage {state.ljust(20, '-')}")
+    finally:
+        pass
 
 
 def callback(ch, method, properties, body):
@@ -109,8 +111,8 @@ def callback(ch, method, properties, body):
         key = body["payload"]["key"]
         value = body["payload"]["value"]
         msg = {key: value}
-        # client.publish(topic, json.dumps(msg), 0)
-        print(msg)
+        client.publish(topic, json.dumps(msg), 0)
+        # print(msg)
         state = "DONE"
         ch.basic_ack(method.delivery_tag)
     except:
@@ -128,7 +130,7 @@ def rabbitmq():
 
 def mqtt():
     try:
-        client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASS)
+        # client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASS)
         if client.connect(settings.MQTT_HOST, settings.MQTT_PORT, 60) != 0:
             print("Couldn't connect to the mqtt broker")
             logging.error("Couldn't connect to the mqtt broker")
@@ -142,6 +144,7 @@ def mqtt():
         client.disconnect()
         logging.error(f"Disconnecting from the MQTT broker")
         print("Disconnecting from the MQTT broker")
+        # sys.exit(0)
         mqtt()
 
 
@@ -154,8 +157,15 @@ if __name__ == "__main__":
         format="%(levelname)s\t%(asctime)s\t%(message)s",
         level=logging.INFO,
     )
+    # import paho.mqtt
 
-    client = paho.Client(client_id=generate_uuid())
+    # client = paho.Client(client_id=generate_uuid())
+    # if paho.mqtt.__version__[0] > "1":
+    client = mqttClinet.Client(
+        mqttClinet.CallbackAPIVersion.VERSION2, client_id=generate_uuid()
+    )
+    # else:
+    #     client = mqttClinet.Client()
     try:
         t1 = Thread(target=rabbitmq)
         t2 = Thread(target=mqtt)
